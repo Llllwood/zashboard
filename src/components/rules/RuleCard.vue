983cd5562e78741057a13764cc74daa23a43957b
Llllwood <144126126+Llllwood@users.noreply.github.com>
2024-12-19 19:01:04 +0800

    Update RuleCard.vue

--- src/components/rules/RuleCard.vue
+++ src/components/rules/RuleCard.vue
@@ -1,22 +1,19 @@
 <template>
-  <div :class="twMerge('card mb-1 gap-1 p-2 text-sm', !rule.payload && 'flex-row gap-0')">
-    <div>
-      <span class="mr-2">{{ index }}.</span>
-      <span v-if="rule.payload">
-        {{ rule.payload }}
-      </span>
-    </div>
-    <div class="flex gap-2">
-      <span :class="rule.payload && 'text-slate-500'">{{ rule.type }}</span>
-      <span>-></span>
-      <span class="text-primary">{{ rule.proxy }}</span>
-    </div>
+  <div class="card mb-1 block p-2 text-sm">
+    <span class="text-r-number mr-2">{{ index }}.</span>
+    <span class="text-r-type">[{{ rule.type }}]</span>
+    <span
+      class="text-r-name"
+      v-if="rule.payload"
+      >{{ rule.payload }}</span
+    >
+    <span class="text-r-arrow">></span>
+    <span class="text-r-proxy">{{ rule.proxy }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Rule } from '@/types'
-import { twMerge } from 'tailwind-merge'
 
 defineProps<{
   rule: Rule
